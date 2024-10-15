cf03414ce99d6f96e0718f19c5e9e054e893190b
Nacho0150 <ignacioibanez01@gmail.com>
2024-10-15 18:02:00 -0300

    todo hecho

--- IntegradorJava/src/main/java/com/miempresa/integradorjava/Main.java
+++ IntegradorJava/src/main/java/com/miempresa/integradorjava/Main.java
@@ -2,6 +2,7 @@ package com.miempresa.integradorjava;
 
 import com.miempresa.integradorjava.modelo.Cliente;
 import com.miempresa.integradorjava.modelo.Turnos;
+import com.miempresa.integradorjava.servicio.Servicios;
 
 import java.util.Scanner;
 
@@ -11,55 +12,94 @@ public class Main {
     System.out.println("        ********** 11 Sueños **********         ");
     System.out.println("================================================");
 
-    Cliente cliente1 = new Cliente("Christian", "Bonilla","chris@gmail.com", "2234422333");
-    Turnos turno1 = new Turnos(23,9,2024,14, cliente1);
-//    System.out.println("cliente1 = " + cliente1);
-    System.out.println(turno1);
-//    Turnos.mostrarHorarios();
+    menu();
+  }
 
-//    menu();
-}
+  public static void menu() {
+    int opcion = 0;
+    Scanner entrada = new Scanner(System.in);
 
-//  public static void menu() {
-//    int opcion = 0;
-//    Scanner entrada = new Scanner(System.in);
-//    while (opcion != 4) {
-//      System.out.println("\n¡Hola! Bienvenido a nuestro menú de servicio\n ¿Cómo podemos ayudarte?\n");
-//      System.out.println("                 >>  MENÚ  <<");
-//      System.out.println("------------------------------------------------");
-//      System.out.println(" (1)  Nuestros servicios");
-//      System.out.println(" (2)  Reserva online");
-//      System.out.println(" (3)  Contacto\n\n");
-//      System.out.println(" (4)  Salir");
-//      System.out.println("------------------------------------------------");
-//      System.out.println();
-//      System.out.print("Elija una opción para continuar: ");
-//
-//      opcion = Integer.parseInt(entrada.nextLine());
-//
-//      switch (opcion) {
-//        case 1:
-//          // Nuestros servicios
-//          System.out.println("1");
-//          break;
-//        case 2:
-//          // Reserva online
-//          System.out.println("2");
-//          break;
-//        case 3:
-//          // Contacto
-//          System.out.println("3");
-//          break;
-//        case 4:
-//          // Mensaje de despedida
-//          System.out.println("4");
-//          break;
-//        default:
-//          System.out.println("Opción no válida. Inténtalo de nuevo.");
-//      }
-//    }
-//      entrada.close();
-//  }
+    // Crear instancias de la clase Servicios
+    Servicios reflexologia = new Servicios(
+            "Reflexología",
+            "Terapia para tratar puntos dolorosos a través de masajes podales.",
+            60,
+            11500
+    );
+
+    Servicios masajeDescontracturante = new Servicios(
+            "Masaje descontracturante",
+            "Ideal para relajar la musculatura y disolver contracturas.",
+            60,
+            21900
+    );
+
+    Servicios pulidoCorporal = new Servicios(
+            "Pulido corporal e hidratación",
+            "Tratamiento corporal hiper estimulante que elimina las células muertas.",
+            60,
+            21900
+    );
+
+    // Imprimir el menú al inicio
+    mostrarMenu();
+
+    while (opcion != 4) {
+      System.out.print("Elija una opción para continuar: ");
+      opcion = Integer.parseInt(entrada.nextLine());
+
+      switch (opcion) {
+        case 1:
+          // Mostrar los servicios disponibles
+          System.out.println("\nNuestros Servicios:");
+          System.out.println("-------------------------------------------");
+            reflexologia.mostrarServicio();
+            masajeDescontracturante.mostrarServicio();
+            pulidoCorporal.mostrarServicio();
+
+          // Pausa para que el usuario vea los servicios
+          System.out.println("\nPresione Enter para volver al menú...");
+          entrada.nextLine();  // Esperar que el usuario presione Enter
+          break;
+
+
+        case 2:
+          // Reserva online
+          System.out.println("\nReserva online:");
+          break;
+        case 3:
+          // Contacto
+          System.out.println("\nContacto:");
+          break;
+        case 4:
+          // Mensaje de despedida
+          System.out.println("\nGracias por su visita. ¡Hasta pronto!");
+          break;
+        default:
+          System.out.println("\nOpción no válida. Inténtalo de nuevo.");
+      }
+
+      // Solo mostramos el menú otra vez si no se selecciona "Salir"
+      if (opcion != 4) {
+        System.out.println();
+        mostrarMenu();
+      }
+    }
+
+    entrada.close();
+  }
+
+  // Método para mostrar el menú principal
+  public static void mostrarMenu() {
+    System.out.println("\n¡Hola! Bienvenido a nuestro menú de servicio\n¿Cómo podemos ayudarte?\n");
+    System.out.println("                 >>  MENÚ  <<");
+    System.out.println("------------------------------------------------");
+    System.out.println(" (1)  Nuestros servicios");
+    System.out.println(" (2)  Reserva online");
+    System.out.println(" (3)  Contacto");
+    System.out.println(" (4)  Salir");
+    System.out.println("------------------------------------------------");
+  }
 }
 
 
--- IntegradorJava/src/main/java/com/miempresa/integradorjava/servicio/ReservarTurno.java
+++ IntegradorJava/src/main/java/com/miempresa/integradorjava/servicio/ReservarTurno.java
@@ -1,5 +1,111 @@
 package com.miempresa.integradorjava.servicio;
 
+import java.util.List;
+
 public class ReservarTurno {
 
+    public void llenarMatrices (List<String> turnos, String[][] cronograma, String[][] reserva) {
+        int cont = 0;
+        int cont1 = 1;
+        int numAzar = 0;
+
+        //Completa los horarios de los turnos
+        turnos.add("   09:00   ");
+        turnos.add("   10:30   ");
+        turnos.add("   12:00   ");
+        turnos.add("   13:30   ");
+        turnos.add("   15:00   ");
+        turnos.add("   16:00   ");
+        turnos.add("   18:00   ");
+
+        for (int i = 0; i < 29; i++) {
+            for (int j = 0; j < 6; j++) {
+                //Se utiliza la función 'Math.random()' para llenar, aleatoreamente, algunas posiciones del cronograma, y
+                //de la sensación de encontrarse con un sitio concurrido.
+                numAzar = (int) (Math.random() * 2) + 1;
+
+                //Condicional para indicar con 'Cerrado', los días sábados y domingos.
+                if (i != cont && i != cont1) {
+                    if (numAzar == 0) {
+                        cronograma[i][j] = "       ";
+                    } else {
+                        cronograma[i][j] = "   R   ";
+                    }
+                } else {
+                    cronograma[i][j] = "Cerrado";
+                }
+            }
+
+            //Contador para que cada 7 dias se coloque "Cerrado", solo si la 'i' es igual a su contador.
+            if  (i == cont) {
+                cont += 7;
+            }
+            if  (i == cont1) {
+                cont += 7;
+            }
+        }
+
+        //Inicializamos todos los lugares de las reservas en "" para que no de error en la comprobación posterior.
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 2; j++) {
+                reserva[i][j] = "";
+            }
+        }
+    }
+
+    public void mostrarTurnos(List<String> turnos, String[][] cronograma) {
+        int numDias = 30;
+        int numTurnos = 7;
+        int cont = 0;
+
+        System.out.println("");
+        // Mostrar encabezados de horarios
+        System.out.println("       | " + turnos.get(0) + " || " + turnos.get(1) + " || " + turnos.get(2) + " || " + turnos.get(3) + " || " + turnos.get(4) + " || " + turnos.get(5) + " || " + turnos.get(6) + " |");
+        for (int i = 0; i < numDias; i++) {
+            for (int j = 0; j < numTurnos; j++) {
+                if (cont == 0) {
+                    System.out.print("       ");
+                }
+                System.out.print("_________");
+                cont++;
+            }
+
+            cont = 0;
+            System.out.println("");
+
+            if (i > 9) {
+                System.out.print("Dia " + i + ":");
+            } else {
+                System.out.print("Dia " + i + ":");
+            }
+
+            for (int j = 0; j < numTurnos; j++) {
+                if (cont == 0) {
+                    System.out.print("");
+                }
+
+                if  (cronograma[i-1][j-1] != "") {
+                    System.out.print("|   " + cronograma[i-1][j-1] + "   |");
+                } else {
+                    System.out.print("|       |");
+                }
+                cont++;
+            }
+
+            cont = 0;
+            System.out.println("");
+
+            if (i == 30) {
+                for (int j = 0; j < numTurnos; j++) {
+                    if (cont == 0) {
+                        System.out.print("       ");
+                    }
+                    System.out.print("_______________");
+                    cont++;
+                }
+            }
+
+            cont = 0;
+        }
+    }
 }
